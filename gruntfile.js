1b66c1c7e08bd4b7266663fb6c68816d65b03eef
Alex <alex_soares_oliveira@hotmail.com>
2024-06-15 11:59:14 -0300

    correção da build

--- gruntfile.js
+++ gruntfile.js
@@ -9,29 +9,40 @@ module.exports = function(grunt) {
         },
         uglify: {
             options: {
-                mangle: false 
+                mangle: false // Evita a minificação de nomes
             },
             my_target: {
                 files: {
-                    'dist/js/script.min.js': ['src/script.js'] 
+                    'dist/js/script.min.js': ['src/script.js']
                 }
             }
         },
         watch: {
             less: {
                 files: ['src/less/**/*.less'],
-                tasks: ['less:production']
+                tasks: ['less:production'],
+                options: {
+                    livereload: true // Ativa o livereload para recarregar o navegador automaticamente
+                }
             },
             js: {
-                files: ['src/script.js'], 
-                tasks: ['uglify']
+                files: ['src/script.js'],
+                tasks: ['uglify'],
+                options: {
+                    livereload: true // Ativa o livereload para recarregar o navegador automaticamente
+                }
             }
         }
     });
 
+    // Carrega plugins do Grunt necessários
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['less:production', 'uglify', 'watch']);
+    // Define a tarefa "build" que engloba less:production, uglify e watch
+    grunt.registerTask('build', ['less:production', 'uglify']);
+
+    // Tarefa padrão, que executa build e watch
+    grunt.registerTask('default', ['build', 'watch']);
 };
