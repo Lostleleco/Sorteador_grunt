c68c10b227da13a1af457b3f7e906785faa8d711
Alex <alex_soares_oliveira@hotmail.com>
2024-06-19 08:20:17 -0300

    resolvendo conflito de deploy

--- gruntfile.js
+++ gruntfile.js
@@ -9,11 +9,22 @@ module.exports = function(grunt) {
         },
         uglify: {
             options: {
-                mangle: false // desativa o mapeamento de nomes das variáveis
+                mangle: true // Corrigido para adicionar valor booleano
             },
             my_target: {
                 files: {
                     'dist/js/script.min.js': ['src/script.js']
+                }
+            }
+        },
+        htmlmin: {
+            options: {
+                removeComments: true, // Exemplo de configuração para htmlmin
+                collapseWhitespace: true
+            },
+            my_target: {
+                files: {
+                    'dist/index.html': 'src/index.html' // Corrigido para minimizar HTML
                 }
             }
         },
@@ -25,6 +36,10 @@ module.exports = function(grunt) {
             js: {
                 files: ['src/script.js'],
                 tasks: ['uglify']
+            },
+            html: {
+                files: ['src/index.html'], // Adicionada a tarefa watch para HTML
+                tasks: ['htmlmin']
             }
         },
         clean: {
@@ -36,7 +51,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-    grunt.registerTask('build', ['clean', 'less:production', 'uglify']);
+    grunt.registerTask('build', ['clean', 'less:production', 'uglify', 'htmlmin']);
     grunt.registerTask('default', ['build', 'watch']);
 };
